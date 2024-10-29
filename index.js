88392312789fea60b770db68ba2e5dcd2b83e880
alejandra081812 <aydalinares4688@gmail.com>
2024-10-28 23:58:47 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
-const icon= document.getElementById("icono")
-const menu=document.getElementById("menu")
+const imagenes = [
+    '/imagenes/imagen1.jpg',
+    '/imagenes/imagen2.jpg',
+    '/imagenes/imagen3.jpg',
+    '/imagenes/imagen4.jpg'
+];
 
-icon.addEventListener("click",function(){
-    menu.classList.toggle("inactivo")
-})
+let indiceActual = 0;
+
+function cambiarImagen(direccion) {
+    indiceActual = (indiceActual + direccion + imagenes.length) % imagenes.length;
+    const imagenPrincipal = document.getElementById('imagen-principal');
+    imagenPrincipal.src = imagenes[indiceActual];
+}
+
+const imagenescon = [
+    '/imagenes/imagen5.jpg',
+    '/imagenes/imagen6.jpg',
+    '/imagenes/imagen7.jpg',
+    '/imagenes/imagen8.jpg'
+];
+
+let indiceActual2 = 0;
+
+function cambiarImagencon(direccion) {
+    indiceActual2 = (indiceActual2 + direccion + imagenescon.length) % imagenescon.length;
+    const imagenPrincipal2 = document.getElementById('imagen-principal2');
+    imagenPrincipal2.src = imagenescon[indiceActual2];
+}
 
